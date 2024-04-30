8202419a85baa8172bfa7f13a9990aa99d357930
Evaldo Cunha F <evaldocunhaf@gmail.com>
2024-04-30 17:47:34 -0300

    criacao da lib tabuleiro

---
+++ tabuleiro.c
@@ -0,0 +1,6 @@
+//
+// Created by Evaldo Cunha Filho on 30/04/24.
+//
+void incluir(){
+
+}
---
+++ tabuleiro.h
@@ -0,0 +1,9 @@
+//
+// Created by Evaldo Cunha Filho on 30/04/24.
+//
+
+#ifndef TABULEIRO_TABULEIRO_H
+#define TABULEIRO_TABULEIRO_H
+
+#endif //TABULEIRO_TABULEIRO_H
+
